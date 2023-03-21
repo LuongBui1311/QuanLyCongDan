caf1916567e3da7e5932877c2af68de4f9c2e0df
Lương Bùi <luonglb1311@gmail.com>
2023-03-21 09:16:40 +0700

    updated

--- QuanLyCongDan/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ QuanLyCongDan/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -80,7 +80,7 @@ namespace QuanLyCongDan {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/QuanLyCongDan;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/QuanLyCongDan;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
